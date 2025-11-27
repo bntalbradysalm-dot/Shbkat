214a5752e4193c6cb3380c64f2fa6809e5440735
MOHAMMED RADI <mohammedradi651@gmail.com>
2025-11-27 06:27:37 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/alwadi/renew/page.tsx
+++ src/app/alwadi/renew/page.tsx
@@ -17,6 +17,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { useFirestore, useUser, useDoc, useMemoFirebase } from '@/firebase';
 import { collection, addDoc, doc } from 'firebase/firestore';

63167ab4f38f011904e739bb07fcc2206248fedb
MOHAMMED RADI <mohammedradi651@gmail.com>
2026-01-09 08:42:08 +0000

    شعار منظومة الوادي https://i.postimg.cc/mgMYL0dm/Screenshot-20260109-114

--- src/app/alwadi/page.tsx
+++ src/app/alwadi/page.tsx
@@ -37,7 +37,7 @@ export default function AlwadiPage() {
       <div className="flex-1 overflow-y-auto p-4">
         <div className="mb-6 overflow-hidden rounded-xl shadow-lg bg-white flex items-center justify-center">
           <Image
-            src="https://i.postimg.cc/tR80r860/090b6af8-e8d8-4825-bddf-8469a3e8a36c.jpg"
+            src="https://i.postimg.cc/mgMYL0dm/Screenshot-20260109-114041-One-Drive.png"
             alt="Alwadi Promotion"
             width={600}
             height={300}

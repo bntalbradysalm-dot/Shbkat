72c2d3a7b2f29f518d1b4d85bafeef205a87f2ac
MOHAMMED RADI <mohammedradi651@gmail.com>
2026-01-10 10:43:01 +0000

    في رصيد بيتي هذا توفر هذه الواجهة مجموعة من الخدمات التي تتيح للشركاء إج

--- src/app/api/baity/route.ts
+++ src/app/api/baity/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-const API_BASE_URL = 'https://apis.okamel.org/api/partner-yem/bill-balance';
+const API_BASE_URL = 'https://apis.okamel.org/api/partner-yem/bill';
 // It's better to store keys in environment variables
 const API_KEY = process.env.BAITY_API_KEY || 'fb845cb5-b835-4d88-8c8e-eb28cc38a2f2';
 

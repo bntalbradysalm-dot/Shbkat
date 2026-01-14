edd1b94a078460b0c1c3ddd525aadb32c7bf9178
MOHAMMED RADI <mohammedradi651@gmail.com>
2026-01-14 13:30:18 +0000

    Try fixing this error: `Runtime ReferenceError: AlertDialogTrigger is no

--- src/app/telecom-services/page.tsx
+++ src/app/telecom-services/page.tsx
@@ -17,6 +17,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { useUser, useFirestore, useDoc, useMemoFirebase } from '@/firebase';
 import { doc, writeBatch, increment, collection } from 'firebase/firestore';

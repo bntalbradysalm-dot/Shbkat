e7ee74cc019899246c3c4391ebdb63fbca42b253
MOHAMMED RADI <mohammedradi651@gmail.com>
2026-01-15 16:34:22 +0000

    ## Error Type Console Error
    ## Error Message
    Failed to fetch from https

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,7 +5,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+      apiKey: 'AIzaSyDjPJlmaSRajhv_CL8mEf_EF_ieIEhqBvk',
     }),
   ],
   model: 'googleai/gemini-2.5-flash',

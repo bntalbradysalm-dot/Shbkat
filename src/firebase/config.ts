4a791b45ae554cdc46591c89ad4d21c7cf489450
MOHAMMED RADI <mohammedradi651@gmail.com>
2026-01-24 12:00:07 +0000

    لالالا - المشكلة الان قاعدة البيانات لا تتصل بالتطبيق

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   "projectId": "studio-239662212-1b7b6",
   "appId": "1:643429607022:web:43614046eb7b665f5aa87f",
   "storageBucket": "studio-239662212-1b7b6.appspot.com",
-  "apiKey": "AIzaSyA7TYAHxiX0qhhJpdg-_WiCbTl1nTsbznQ",
+  "apiKey": "AIzaSyDjPJlmaSRajhv_CL8mEf_EF_ieIEhqBvk",
   "authDomain": "studio-239662212-1b7b6.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "643429607022"

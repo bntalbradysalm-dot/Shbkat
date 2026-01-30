604f2c87f50abed803c4ad3cfeb1c1fe47f6dea0
MOHAMMED RADI <mohammedradi651@gmail.com>
2026-01-30 12:16:24 +0000

    OPERATION STATUS url: http://DomainName/info?userid=Userid&mobile=xxxx&

--- src/app/api/telecom/route.ts
+++ src/app/api/telecom/route.ts
@@ -59,6 +59,7 @@ export async function POST(request: Request) {
                 apiRequestBody.action = action;
                 break;
             case 'status':
+            case 'balance':
                 endpoint = '/info';
                 apiRequestBody.action = action;
                 break;

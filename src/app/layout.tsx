6c95d5ef6c82d4fcd745a304b212ebda75f24fe0
MOHAMMED RADI <mohammedradi651@gmail.com>
2026-02-11 21:04:37 +0000

    Try fixing this error: `Console Error: Invalid DOM property `crossorigin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <meta name="description" content="Your Digital Wallet" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased bg-background">

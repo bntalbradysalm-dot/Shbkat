6917d1f0f5159dfb4ee3e326d69f454b18949589
MOHAMMED RADI <mohammedradi651@gmail.com>
2026-02-19 11:34:11 +0000

    نطاق اشحن لي https://echehanlyw.yrbso.net/

--- src/app/api/telecom/route.ts
+++ src/app/api/telecom/route.ts
@@ -7,8 +7,8 @@ import CryptoJS from 'crypto-js';
 const USERID = '23207';
 const USERNAME = '770326828';
 const PASSWORD = '770326828moh';
-// الرابط الأساسي المعتمد للسيرفر
-const API_BASE_URL = 'https://echehanly.yrbso.net/api/yr/'; 
+// الرابط الأساسي المعتمد للسيرفر بعد تحديث النطاق
+const API_BASE_URL = 'https://echehanlyw.yrbso.net/api/yr/'; 
 
 /**
  * وظيفة إنشاء الرمز المميز (Token) المطلوبة من المزود
